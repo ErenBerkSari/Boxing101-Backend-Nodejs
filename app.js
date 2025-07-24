c61587ce1e4bd800a5c00953e917f7d4e3e0ca49
ErenBerkSari <erenmxc@gmail.com>
2025-07-24 16:51:48 +0300

    Vercel config

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://boxing101.netlify.app",
+    origin: "https://boxing101.vercel.app",
     credentials: true,
   })
 );
